91c7c87ffaedfb8bd2615a3d0af60123f62ad31a
leila valencia <levamo26@outlook.com>
2022-07-02 09:31:54 -0500

    cambios DL

---
+++ src/DL.java
@@ -0,0 +1,17 @@
+public class DL {
+    public static void main (String [] args){
+    int num =1;
+    String letra;
+    while (num <= 100){
+        if (num % 2 == 0) {
+            letra = "l";
+        } else {
+            letra = "D";
+        }
+                System.out.println(num + "-" + letra);
+                num = num + 1;
+            }
+        }
+        }
+
+
